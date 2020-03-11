115caa128ab5593c25444ef7c3578d569241edef
MoGrass <1951815514@qq.com>
2020-03-11 20:00:47 +0800

    Site updated: 2020-03-11 20:00:45

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,16 +1,16 @@
-<!--崩溃欺骗-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '?你应该是一场梦';
+         document.title = 'QAQ快回来!~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '☀我应该是一阵风' + OriginTitle;
+         document.title = '(ฅ>ω<*ฅ) 回来了呢~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
      }
+ });
